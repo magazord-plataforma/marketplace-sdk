68fd73e95bc38cd04ea238ba55edcaafb907cfd0
MagaZord - Soluções para e-commerce <webmaster@magazord.com.br>
2019-09-26 14:34:51 -0300

    Mensagens/alertas portal

--- src/MagaMarketplace/Domain/Portal/AlertListFilter.php
+++ src/MagaMarketplace/Domain/Portal/AlertListFilter.php
@@ -14,6 +14,8 @@ class AlertListFilter extends Domain\Filter\ListFilter
 
     protected $id;
     protected $title;
+    protected $date;
+    protected $sellerId;
 
     public function getId()
     {
@@ -33,6 +35,26 @@ class AlertListFilter extends Domain\Filter\ListFilter
     public function setTitle($title)
     {
         $this->title = $title;
+    }
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    public function getSellerId()
+    {
+        return $this->sellerId;
+    }
+
+    public function setSellerId($sellerId)
+    {
+        $this->sellerId = $this->intValue($sellerId);
     }
 
 }
